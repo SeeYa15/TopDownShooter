d3b1ad016543b26b7add617fb329dc77ce82a4d5
Johnny <Johnny.Svensson15@gmail.com>
2019-07-14 19:29:42 +0200

    First Commit to Project

---
+++ TopDownShooter/EntityManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooter
+{
+    abstract class EntityManager
+    {
+        protected Texture2D Image { get; set; }
+        protected Vector
+
+        protected void Update()
+        {
+
+        }
+    }
+}
---
+++ TopDownShooter/Resources/Entity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooter
+{
+    abstract class Entity
+    {
+        public Vector2 Position { get; set; }
+        public Texture2D Sprite { get; private set; }
+        public void Draw()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
